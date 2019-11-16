f860d23e944e26cc16c70bf63a2598962e645c11
david07_mc <damoca07@gmail.com>
2019-11-16 17:51:10 +0100

    Iteration 3 finished

---
+++ ISO2/Iteration3/src/main/java/Domain/PieceController.java
@@ -0,0 +1,15 @@
+package Domain;
+
+/**
+ * This class it's the intermediary between the GUI and the information. It has methods to generate a list of pieces if doesn't exists yet and if it does, it has another method to adding new pieces. Both can be useful for 2 use cases, "generate a piece list" and "ask for bill", as they are both lists (the only thing is that they have different content).
+ */
+public class PieceController {
+
+	public void generatePiecesList() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void addPiece(Piece aPiece) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ ISO2/Iteration3/src/main/java/Presentation/Frame_Mechanic.java
@@ -0,0 +1,7 @@
+package Presentation;
+
+/**
+ * Interface for the mechanics tu use (and interact with its functions).
+ */
+public class Frame_Mechanic {
+}
